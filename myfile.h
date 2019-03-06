26d315f12f665226b660b2fac3cf138012193aba
markandevansh99 <500069794@stu.upes.ac.in>
2019-03-06 12:11:55 +0530

    Committing(class in 1st file,empty second file,addition in 3rd file)

---
+++ myfile.h
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+class student
+{
+	 int roll;
+	 char grade;
+	public:
+	 void input()
+	 {
+		  cout<<"\nEnter details";
+		  cin>>roll>>grade;
+	}
+
+	 void output()
+	 {
+		  cout<<roll<grade;
+	}
+
+};
---
+++ myfile3.h
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	 int a=5,b=7,c;
+	 c=a+b;
+	 cout<<"addition is"<<c;
+return 0;
+}
